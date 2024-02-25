3ff61824d96951e5f85a832cd5a06b8bf34747fc
yashmithakode <228w1a1298@vrsec.ac.in>
2024-02-25 11:13:05 +0530

    week1

--- week1.R
+++ week1.R
@@ -8,3 +8,66 @@ df<-data.frame(student_name,age,weight,height,gender)
 df
 df$gender=ifelse(df$gender=="MALE","FEMALE","MALE")
 df
+
+#exercise--2
+working_status<-c("yes","no","yes")
+df1<-data.frame(row.names=student_name,working_status)
+df1
+data<-cbind(df1,df)
+data
+nrow(data)
+ncol(data)
+
+#exercise--3
+class(state.center)
+data.state <- as.data.frame(state.center)
+
+#exercise--4
+a <- c("priya", "anusha", "sandhya")
+b <- c(92, 49, 95)
+c <- c("PASS", "FAIL", "PASS")
+df2 <- data.frame(a, b, c)
+df3 <- df2[order(df2$a), ]
+print(df3)
+
+#exercise--5
+matrix1 <- matrix(1:20, nrow = 5, ncol = 4)
+dataframe <- data.frame(matrix1)
+colnames(dataframe) <- paste("variable_", 1:ncol(dataframe))
+rownames(dataframe) <- paste("id_", 1:nrow(dataframe))
+dataframe
+
+#exercise--6
+class(VADeaths)
+df4<-data.frame(VADeaths)
+df4$total<-rowSums(VADeaths)
+df4 <- df4[,c(5,1,2,3,4)]
+df4
+
+#exercise--7
+class(state.x77)
+df5 <- data.frame(state.x77)
+nrow(df5[df5$Income < 4300,])
+row.names(df5[which.max(df5$Income),])
+
+#exercise--8
+df6 <- data.frame(swiss[c(1,2,3,10,11,12,13),c("Examination", "Education", "Infant.Mortality")])
+df6$Infant.Mortality[4] <- NA
+Total <- colSums(df6, na.rm = TRUE) 
+df7 <- rbind(df6,Total)
+rownames(df7) = c(rownames(df6),"Total")
+df7
+Prop <- df4$Examination / df7["Total","Examination"]
+Prop
+
+#exercise--9
+data1 <- data.frame(state.abb, state.area, state.division, state.region, row.names = state.name)
+colnames(data1) <- substr(colnames(data1), 7, 9)
+
+#exercise--10
+new.df <- cbind(state.x77,df)
+new.df$div <- NULL
+new.df <- subset(new.df,-c(4, 6, 7, 9, 10))
+new.df$Illiteracy.Levels <- ifelse(new.df$Illiteracy >= 0 & new.df$Illiteracy < 1, "Low",
+                                   ifelse(new.df$Illiteracy >= 1 & new.df$Illiteracy < 2, "Some",
+                                          "High"))
