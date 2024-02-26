4977418aca49d2056ac5fc2d07a50944a2c03938
yashmithakode <228w1a1298@vrsec.ac.in>
2024-02-26 11:14:25 +0530

    csv

---
+++ csv.R
@@ -0,0 +1,19 @@
+#Performing operations on csv file
+
+#To read csv file 
+data<-read.csv("C:/Users/pooji/Downloads/mental_health_data.csv")
+data
+
+#To get first 6 rows in the csv file
+head(mental_health_data)
+
+#To get last 6 rows of the file
+tail(mental_health_data)
+
+#To find no.of rows
+nrow(mental_health_data)
+
+#To find the no.of columns
+ncol(mental_health_data)
+
+
